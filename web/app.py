cadbf37b1ddd11cacc3ae21874a90e3d54849415
notdiff <116492863+notdiff@users.noreply.github.com>
2023-06-25 08:58:13 +0300

    Add files via upload

--- web/app.py
+++ web/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request, send_file, url_for
+from flask import Flask, render_template, request, send_file, url_for, jsonify
 from model import CustomTextClassifier
 from process_utils import predict, clear_text
 from torch import load as torchload
 from torch import device as torchdevice
+import pandas as pd
+from random import choice
 import os
+import io
 
 app = Flask(__name__)
 
+randoms_vacs = list(pd.read_csv('final.csv')['responsobilities'])
 classifier = CustomTextClassifier('cointegrated/rubert-tiny', 'cointegrated/rubert-tiny', n_classes=3)
-classifier.model = torchload('models\\ruBERT_by_ALTF4.pt', map_location=torchdevice('cpu'))
+classifier.model = torchload('/home/UFOHACK/models/ruBERT_by_ALTF4.pt', map_location=torchdevice('cpu'))
 
 idx2labels = {
     0: 'Требования',
@@ -33,6 +37,13 @@ def result():
 
     return render_template('result.html', duties=duties, conditions=conditions, requirements=requirements)
 
+@app.route('/get_random')
+def return_vac():
+    response = jsonify({'text': choice(randoms_vacs)})
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 @app.route('/help')
 def support():
     return render_template('help.html')
@@ -42,47 +53,52 @@ def download():
     duties = request.form['duties']
     conditions = request.form['conditions']
     requirements = request.form['requirements']
-    filename = request.form['filename'] + ".txt"
-    result_text = f"Должностные обязанности: {duties}\nУсловия: {conditions}\nТребования к соискателю: {requirements}"
-    with open(filename, 'w') as file:
-        file.write(result_text)
+    filename = 'output.xlsx'
+
+    data = pd.DataFrame(data={'Обязанности': [duties], 'Требования': [requirements], 'Условия': [conditions]})
+    data.to_excel(filename, index=False)
+
     return send_file(filename, as_attachment=True)
 
-import pandas as pd
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
-    if file.filename.endswith('.csv'):
-        file.save(file.filename)
-        df = pd.read_csv(file.filename)
-        if 'Должностные обязанности' in df.columns:
-            data = []
-            for input_text in df['Должностные обязанности']:
-                input_text = clear_text(input_text)
-                classes = predict(classifier, input_text, idx2labels)
+    file_extension = os.path.splitext(file.filename)[1].lower()
 
-                classes['Обязанности'] = '. '.join(classes['Обязанности'])
-                classes['Условия'] = '. '.join(classes['Условия'])
-                classes['Требования'] = '. '.join(classes['Требования'])
-
-                data.append(classes)
-
-            df = pd.DataFrame.from_records(data)
-            output_filename = 'output.csv'
-            df.to_csv(output_filename, index=False)
-            return send_file(output_filename, as_attachment=True)
-        else:
-            return "Файл CSV не содержит столбец 'Должностные обязанности'."
+    if file_extension == '.csv':
+        df = pd.read_csv(io.StringIO(file.read().decode('utf-8')))
+    elif file_extension in ['.xls', '.xlsx']:
+        df = pd.read_excel(io.BytesIO(file.read()))
     else:
-        return "Неправильный формат файла. Пожалуйста, загрузите файл в формате CSV."
+        return "Неправильный формат файла. Пожалуйста, загрузите файл в формате CSV или Excel."
 
+    if 'Должностные обязанности' in df.columns:
+        data = []
+        for input_text in df['Должностные обязанности']:
+            input_text = clear_text(input_text)
+            classes = predict(classifier, input_text, idx2labels)
 
-def result_from_text(text):
-    duties = text[::3]
-    conditions = text[1::3]
-    requirements = text[2::3]
-    return render_template('result.html', duties=duties, conditions=conditions, requirements=requirements)
+            classes['Обязанности'] = '. '.join(classes['Обязанности'])
+            classes['Условия'] = '. '.join(classes['Условия'])
+            classes['Требования'] = '. '.join(classes['Требования'])
+
+            data.append(classes)
+
+        df = pd.DataFrame.from_records(data)
+
+        output_filename = 'output'
+        if file_extension == '.csv':
+            output_filename += '.csv'
+            df.to_csv(output_filename, index=False, encoding='windows-1251')
+        else:
+            output_filename += '.xlsx'
+            df.to_excel(output_filename, index=False)
+
+        return send_file(output_filename, as_attachment=True)
+    else:
+        return "Файл не содержит столбец 'Должностные обязанности'."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
